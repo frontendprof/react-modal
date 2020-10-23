c3cc43dc7a096acd056ebdd369ab31337d402db8
frontendprof <abdumaliksharipov@gmail.com>
2020-10-23 22:35:25 +0500

    Applying style with styled-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 
 import React from "react";
+import styled from "styled-components";
 
 import './App.css';
+
+
+
+const Container=styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  height:100vh;
+`
+
+const Button=styled.button`
+  min-width:100px;
+  padding:16px 32px;
+  border-radius:5px;
+  border:none;
+  background:#141414;
+  color:#fff;
+  font-size:24px;
+  cursor:pointer; 
+`
+
+
+
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Peace</h1>
+      <Container>
+        <Button>I am a Modal</Button>
+      </Container>
     </div>
   );
 }
